2039fdd9ee69c7b08ac023846097083d676aeddb
Francesco Martinuzzi <martinuzzi.francesco@gmail.com>
2020-08-01 16:35:11 +0200

    added irrational sign input matrix

--- src/esn_input_layers.jl
+++ src/esn_input_layers.jl
@@ -50,3 +50,36 @@ function min_complex_input(res_size::Int,
     end
     return W_in
 end 
+
+#from "minimum complexity echo state network" Rodan 
+#and "simple deterministically constructed cycle reservoirs with regular jumps" by Rodan and Tino
+function irrational_sign_input(res_size::Int,
+        in_size::Int,
+        weight::Float64;
+        irrational::Irrational = pi)
+    
+    setprecision(BigFloat, Int(ceil(log2(10)*(res_size*in_size+1))))
+    ir_string = string(BigFloat(irrational)) |> collect
+    deleteat!(ir_string, findall(x->x=='.', ir_string))
+    ir_array = Array{Int}(undef, length(ir_string))
+    W_in = Array{Float64}(undef, res_size, in_size)
+
+    for i =1:length(ir_string)
+        ir_array[i] = parse(Int, ir_string[i])
+    end    
+    
+    counter = 0
+    
+    for i=1:res_size
+        for j=1:in_size
+            counter += 1
+            println
+            if ir_array[counter] < 5
+                W_in[i, j] = -weight
+            else
+                W_in[i, j] = weight
+            end
+        end
+    end
+    return W_in
+end
