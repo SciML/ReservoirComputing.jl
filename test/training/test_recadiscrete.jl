5e56a13c94299acc18f3d625e50282dc9c22c22b
Francesco Martinuzzi <martinuzzi.francesco@gmail.com>
2020-08-08 16:33:01 +0200

    added more tests for gol reca

--- test/training/test_recadiscrete.jl
+++ test/training/test_recadiscrete.jl
@@ -8,12 +8,6 @@ permutations = 2
 
 reca = RECA_discrete(train_data, rule, generations, expansion_size, permutations)
 
-@test isequal(reca.train_data, train_data)
-@test isequal(reca.rule, rule)
-@test isequal(reca.generations, generations)
-@test isequal(reca.expansion_size, expansion_size)
-@test isequal(reca.permutations, permutations)
-
 W_out = ESNtrain(reca, 0.001, train_data = ones(Float64, 2, 10))
 output = RECAdirect_predict_discrete(reca, W_out, ones(Int, 2, 10))
 
