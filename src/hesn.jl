dde9c13ea4bb796cfbee7459a4cdd7477d6380a6
Anas <anasabdelrehim@gmail.com>
2020-11-07 20:36:21 -0500

    fixed bug, refactor constructor to include needed fields for prediction

--- src/hesn.jl
+++ src/hesn.jl
@@ -1,12 +1,16 @@
 abstract type AbstractHESN <: AbstractEchoStateNetwork end
 
-struct HESN{T, S<:AbstractArray{T}, I, B, F, N, M} <: AbstractHESN
+struct HESN{T, S<:AbstractArray{T}, I, B, F, N, M, U} <: AbstractHESN
     res_size::I
     in_size::I
     out_size::I
     train_data::S
     physics_model_data::S
     prior_model::M
+    u0::U
+    tspan::Tuple
+    datasize::I
+    dt::T
     alpha::T
     nla_type::N
     activation::F
@@ -30,10 +34,10 @@ function HESN(W::AbstractArray{T},
 
     #Create physics data with one extra step ahead extra
     trange = collect(range(tspan[1], tspan[2], length = datasize))
-    dt = tspan[2]-trange[1]
+    dt = trange[2]-trange[1]
     tsteps = push!(trange, dt + trange[end])
-    tspan = (tspan[1], dt+tspan[2])
-    physics_model_data = prior_model(u0, tspan, tsteps)
+    tspan_new = (tspan[1], dt+tspan[2])
+    physics_model_data = prior_model(u0, tspan_new, tsteps)
     physics_informed_data = vcat(train_data, physics_model_data[:, 1:end-1])
 
     in_size = size(physics_informed_data, 1)
@@ -53,6 +57,7 @@ function HESN(W::AbstractArray{T},
         typeof(extended_states),
         typeof(activation),
         typeof(nla_type),
-        typeof(prior_model)}(res_size, in_size, out_size, train_data, physics_model_data,
-        prior_model, alpha, nla_type, activation, W, W_in, states, extended_states)
+        typeof(prior_model),
+        typeof(u0)}(res_size, in_size, out_size, train_data, physics_model_data,
+        prior_model, u0, tspan, datasize, dt, alpha, nla_type, activation, W, W_in, states, extended_states)
 end
