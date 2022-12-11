5a53451d21d9527d0df3256b5f1e5e4b183fe124
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-12-11 02:17:56 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,6 +10,14 @@ include("pages.jl")
 makedocs(modules = [ReservoirComputing],
          clean = true, doctest = false,
          sitename = "ReservoirComputing.jl",
+         strict = [
+             :doctest,
+             :linkcheck,
+             :parse_error,
+             :example_block,
+             # Other available options are
+             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
          format = Documenter.HTML(analytics = "UA-90474609-3",
                                   assets = ["assets/favicon.ico"],
                                   canonical = "https://docs.sciml.ai/ReservoirComputing/stable/"),
