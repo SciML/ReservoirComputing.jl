b1ba13131c47e6f76cff08c124c2d9c612e8f481
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2025-03-10 10:59:44 +0100

    added cycle reservoir with self loops

--- src/ReservoirComputing.jl
+++ src/ReservoirComputing.jl
@@ -39,7 +39,8 @@ export StandardRidge
 export scaled_rand, weighted_init, informed_init, minimal_init, chebyshev_mapping,
        logistic_mapping, modified_lm
 export rand_sparse, delay_line, delay_line_backward, cycle_jumps,
-       simple_cycle, pseudo_svd, chaotic_init, low_connectivity, double_cycle
+       simple_cycle, pseudo_svd, chaotic_init, low_connectivity, double_cycle,
+       self_loop_cycle
 export RNN, MRNN, GRU, GRUParams, FullyGated, Minimal
 export train
 export ESN, HybridESN, KnowledgeModel, DeepESN
