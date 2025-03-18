37acc4707b830f685f431c3fa8669f24e8612d69
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2025-03-18 10:31:01 +0100

    docs: add more detailed kwargs for minimal inits

--- src/esn/esn_inits.jl
+++ src/esn/esn_inits.jl
@@ -593,6 +593,9 @@ Create and return a delay line reservoir matrix [^rodan2010].
 # Keyword arguments
 
   - `weight`: Determines the value of all connections in the reservoir.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the sub-diagonal
+    you want to populate.
     Default is 0.1.
   - `shift`: delay line shift. Default is 1.
   - `return_sparse`: flag for returning a `sparse` matrix.
@@ -665,10 +668,17 @@ Creates a matrix with backward connections as described in [^rodan2010].
 # Keyword arguments
 
   - `weight`: The weight determines the absolute value of
-    forward connections in the reservoir. Default is 0.1
-
+    forward connections in the reservoir. 
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the sub-diagonal
+    you want to populate.
+    Default is 0.1
   - `fb_weight`: Determines the absolute value of backward connections
-    in the reservoir. Default is 0.2
+    in the reservoir.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the sub-diagonal
+    you want to populate.
+    Default is 0.2
   - `return_sparse`: flag for returning a `sparse` matrix.
     Default is `false`.
   - `delay_kwargs` and `fb_kwargs`: named tuples that control the kwargs for the
@@ -739,9 +749,14 @@ Create a cycle jumps reservoir [^Rodan2012].
 # Keyword arguments
 
   - `cycle_weight`:  The weight of cycle connections.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the cycle
+    you want to populate.
     Default is 0.1.
-
   - `jump_weight`: The weight of jump connections.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the jumps
+    you want to populate.
     Default is 0.1.
   - `jump_size`:  The number of steps between jump connections.
     Default is 3.
@@ -816,6 +831,9 @@ Create a simple cycle reservoir [^rodan2010].
 # Keyword arguments
 
   - `weight`: Weight of the connections in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the cycle
+    you want to populate.
     Default is 0.1.
   - `return_sparse`: flag for returning a `sparse` matrix.
     Default is `false`.
@@ -1282,8 +1300,14 @@ W_{i,j} =
 # Keyword arguments
 
   - `cycle_weight`: Weight of the cycle connections in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the cycle
+    you want to populate.
     Default is 0.1.
   - `selfloop_weight`: Weight of the self loops in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the diagonal
+    you want to populate.
     Default is 0.1.
   - `return_sparse`: flag for returning a `sparse` matrix.
     Default is `false`.
@@ -1366,6 +1390,9 @@ W_{i,j} =
 # Keyword arguments
 
   - `cycle_weight`: Weight of the cycle connections in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the cycle
+    you want to populate.
     Default is 0.1.
   - `selfloop_weight`: Weight of the self loops in the reservoir matrix.
     Default is 0.1.
@@ -1448,8 +1475,14 @@ W_{i,j} =
 # Keyword arguments
 
   - `weight`: Weight of the cycle connections in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the cycle
+    you want to populate.
     Default is 0.1.
   - `selfloop_weight`: Weight of the self loops in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the diagonal
+    you want to populate.
     Default is 0.1.
   - `return_sparse`: flag for returning a `sparse` matrix.
     Default is `false`.
@@ -1536,8 +1569,14 @@ W_{i,j} =
 # Keyword arguments
 
   - `weight`: Weight of the cycle connections in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the cycle
+    you want to populate.
     Default is 0.1.
   - `selfloop_weight`: Weight of the self loops in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the diagonal
+    you want to populate.
     Default is 0.1.
   - `return_sparse`: flag for returning a `sparse` matrix.
     Default is `false`.
@@ -1619,9 +1658,23 @@ W_{i,j} =
 # Keyword arguments
 
   - `weight`: Weight of the cycle connections in the reservoir matrix.
+    This can be provided as a single value or an array. In case it is provided as an
+    array please make sure that the lenght of the array matches the lenght of the sub-diagonal
+    you want to populate.
     Default is 0.1.
   - `return_sparse`: flag for returning a `sparse` matrix.
     Default is `false`.
+  - `sampling_type`: Sampling that decides the distribution of `weight` negative numbers.
+      If set to `:no_sample` the sign is unchanged. If set to `:bernoulli_sample!` then each
+      `weight` can be positive with a probability set by `positive_prob`. If set to
+      `:irrational_sample!` the `weight` is negative if the decimal number of the
+      irrational number chosen is odd. Default is `:no_sample`.
+  - `positive_prob`: probability of the `weight` being positive when `sampling_type` is
+      set to `:bernoulli_sample!`. Default is 0.5
+  - `irrational`: Irrational number whose decimals decide the sign of `weight`.
+      Default is `pi`.
+  - `start`: Which place after the decimal point the counting starts for the `irrational`
+      sign counting. Default is 1.
 
 # Examples
 
